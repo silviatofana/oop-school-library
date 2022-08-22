5e0d63d9542433daaac24aa93ffd02976371e095
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-22 14:37:19 +0300

    requires changes

--- person.rb
+++ person.rb
@@ -3,7 +3,7 @@ class Person
   attr_reader :id
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
-    @id = id
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
