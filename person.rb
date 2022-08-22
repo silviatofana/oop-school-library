8e02b40fa773416858003abe3cbbf760cc9f97b7
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-22 19:40:24 +0300

    update person.rb

--- person.rb
+++ person.rb
@@ -4,8 +4,8 @@ class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name: 'Unknown', parent_permission: true)
-    @id = id
+  def initialize(age:, name: 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
@@ -27,6 +27,3 @@ class Person
     @age >= 18
   end
 end
-
-person1 = Person.new(11, 'Gift', false)
-puts person1.can_use_services?
