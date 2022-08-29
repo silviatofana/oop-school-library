b97dc24669091751f7d018b63bc85f768bf866ca
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-29 20:57:17 +0300

    delete

--- utils/capital_decorator.rb
+++
@@ -1,9 +0,0 @@
-require './base_decorator'
-
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-# the capitalize decorator that will inherit from the base decorator
