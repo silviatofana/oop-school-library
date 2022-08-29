5884d219e07afc1bdf0e3feed4a29622ea943521
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-29 21:02:20 +0300

    rentals.rb file models folder

---
+++ models/rental.rb
@@ -0,0 +1,15 @@
+require './models/person'
+
+class Rental
+  attr_accessor :date, :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+
+    @person = person
+    person.rentals << self
+
+    @book = book
+    book.rentals << self
+  end
+end
