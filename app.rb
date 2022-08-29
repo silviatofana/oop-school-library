3d843cc03ac998337edf8fb60a72fa3ed80fd621
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-08-29 18:58:23 +0300

    update

--- app.rb
+++ app.rb
@@ -1,110 +1,21 @@
-require_relative './book'
-require_relative './student'
-require_relative './teacher'
-require_relative './rental'
-require './separations/books_app'
+require_relative 'options'
 
 class App
-  attr_accessor :rentals, :books, :people
-
   def initialize
-    @books = []
-    @people = []
-    @rentals = []
-    @books = BookModule.new(@books)
+    # it create an instance of option
+    @options = Options.new
   end
 
-  def list_all_people
-    puts 'No people exist' if @people.empty?
-    @people.each_with_index do |person, index|
-      puts "(#{index + 1}) [#{person.class}] => Name: #{person.name}, Age: #{person.age}, Id: #{person.id}"
+  def run
+    puts 'WELCOME TO MANDELA LIBRARY APP! '
+    loop do
+      @options.print_options
+      operation = gets.chomp
+      exit if operation == 'q'
+
+      @options.choose_option operation
     end
-  end
-
-  def create_person()
-    puts 'Do you want create student🧑‍🎓 (1) or teacher🧑‍🏫 (2)? [Enter the number]: '
-    person_type = gets.chomp.to_i
-    case person_type
-    when 1
-      print 'Enter student name: '
-      name = gets.chomp
-      print 'Enter age: '
-      age = gets.chomp
-      @people.push(Student.new('classroom', age, name))
-      puts "#{name.capitalize} added successfully 💓"
-    when 2
-      print 'Enter teacher name: '
-      name = gets.chomp
-      print 'Enter age: '
-      age = gets.chomp
-      print 'Enter specialization: '
-      specialization = gets.chomp
-      @people.push(Teacher.new(specialization, age, name))
-      puts "#{name.capitalize} added successfully 💓"
-    end
-  end
-
-  def create_rental()
-    puts 'Select a book from the following list by the number:📖'
-    list_all_books
-    book_number = gets.chomp.to_i
-    puts ' '
-    puts 'Select a person from the following list by the number:🧑‍🎓🧑‍🏫'
-    list_all_people
-    person_number = gets.chomp.to_i
-    print 'Date: '
-    date = gets.chomp
-    @rentals.push(Rental.new(date, @people[person_number - 1], @books[book_number - 1]))
-    puts 'Rental created successfully 😊'
-  end
-
-  def list_all_rentals
-    print 'ID of person: '
-    id = gets.chomp.to_i
-    puts 'Rentals Books:'
-    @rentals.each do |rental|
-      if rental.person.id == id
-        puts "Peson: #{rental.person.name}  Date: #{rental.date}, Book: '#{rental.book.title}' by #{rental.book.author}"
-      else
-        puts
-        puts 'No Rentals found for the given ID'
-      end
-    end
-  end
-
-  def main_menu
-    puts ' '
-    puts 'Please choose an option by entering a number:
-        1 - List all books
-        2 - List all people
-        3 - Create a person
-        4 - Create a book
-        5 - Create a rental
-        6 - List all rentals for a given person id
-        7 - Exit'
-    puts ' '
-  end
-
-  def menu_selection
-    main_menu
-    selection = gets.chomp.to_i
-    case selection
-    when 1
-      @books.list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_person
-    when 4
-      @books.create_book
-    when 5
-      create_rental
-    when 6
-      list_all_rentals
-    when 7
-      puts 'bye bye see you later 👋'
-      exit
-    end
-    menu_selection
+    puts 'thank you for using the app'
   end
 end
+
